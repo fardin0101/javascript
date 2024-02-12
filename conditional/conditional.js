1e1f1e823f4c2f1f195ddc5e91fd13d2b027a677
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-12 21:35:54 +0600

    commit

---
+++ conditional/conditional.js
@@ -0,0 +1,13 @@
+//  console.log(5<20);
+// console.log(19>=30);
+// var peyaraPrice = 30;
+// var applePrice = 48;
+
+// console.log(applePrice > peyaraPrice);
+
+// difference between == and ===
+// console.log(33 == "33"); //this will compare only value
+// console.log(33 === "33"); // this wicompare value as well as daratype
+
+// not equal sign !=
+// console.log(33 != 33);
