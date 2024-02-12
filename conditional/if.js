e60686e44f0aa9a3dfecec08f2960947f9f38ead
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-12 21:48:45 +0600

    fjhds

---
+++ conditional/if.js
@@ -0,0 +1,12 @@
+/* structure
+if (condition) {
+    
+}
+*/
+// var birianiPrice = 400;
+// if (birianiPrice < 300) {  //conditon true hoile if e dukbe 
+//     console.log("mama give me some biriani"); 
+// }
+// else {
+//     console.log("mama muri khawan");
+// }
