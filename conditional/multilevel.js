ccadbd32f1f61cc88de866d9ded5a98bfe554d2d
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-12 22:25:39 +0600

    commit

--- conditional/multilevel.js
+++ conditional/multilevel.js
@@ -1,10 +1,31 @@
-const price = 5000;
+// const price = 5000;
+// if (price >= 5000) {
+//      const discount = price * 10/100 ;
+//      const payAmount = price - discount;
+//      console.log(payAmount);    
+// } else {
+//     console.log("you have to pay" + price);
+// }
+
+
+
+const price = 4000;
+
 if (price >= 5000) {
      const discount = price * 10/100 ;
      const payAmount = price - discount;
      console.log(payAmount);    
-} else {
+}
+    else if (price >= 3500 ){
+        const discount = price * 5/100 ;
+        const payAmount = price - discount;
+        console.log(payAmount);  
+
+    }
+ else {
     console.log("you have to pay" + price);
 }
 
-
+git add .
+git commit -m "commit "
+git push
