833f858bcfbda07205712fbad84870e57f6ed5ab
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-12 22:19:58 +0600

    commit

---
+++ conditional/multilevel.js
@@ -0,0 +1,10 @@
+const price = 5000;
+if (price >= 5000) {
+     const discount = price * 10/100 ;
+     const payAmount = price - discount;
+     console.log(payAmount);    
+} else {
+    console.log("you have to pay" + price);
+}
+
+
--- conditional/multiple.js
+++ conditional/multiple.js
@@ -1,8 +1,17 @@
 // && and 
 // || or
+// const salary = 59000;
+// const height = 60;
+// if (salary > 39999 || height > 70) {
+//     console.log(' shuuu patro ')
+// } else {
+//     console.log('na na hobe nah');   
+// }
+const icBcs= true;
+
 const salary = 59000;
 const height = 60;
-if (salary > 39999 || height > 70) {
+if (salary > 39999 || height > 70 && icBcs === true) {
     console.log(' shuuu patro ')
 } else {
     console.log('na na hobe nah');   
