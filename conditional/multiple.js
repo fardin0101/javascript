b6903d3dd58a8b319fb90be0d5b0b6468242e1f1
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-12 21:56:22 +0600

    fjhds

---
+++ conditional/multiple.js
@@ -0,0 +1,9 @@
+// && and 
+// || or
+const salary = 59000;
+const height = 60;
+if (salary > 39999 || height > 70) {
+    console.log(' shuuu patro ')
+} else {
+    console.log('na na hobe nah');   
+}
