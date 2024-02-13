72f342458e1a9b4e00d19eb4fef44fbd61233914
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-13 15:22:21 +0600

    commit

---
+++ conditional/nested.js
@@ -0,0 +1,12 @@
+const money = 70;
+if (money >300 ){
+    console.log("broo you are richhhhhhh");
+}
+ else {
+        if(money > 100){
+            console.log("toi goribbbbbbbb");
+        }
+        else{
+            console.log("kola khaa")
+        }
+ }
---
+++ conditional/ternary.js
@@ -0,0 +1,19 @@
+// ternary means theee partssss
+/*
+const age = 30;
+
+if(age >18 ){
+    console.log("you can vote");
+}
+else {
+    console.log("jaw baccha ghumai thako");
+}
+*/
+//  ?        :
+// condition ?  do someething when true : do something false
+const age = 30;
+// age >= 10 ? console.log("vote dio") : console.log(" ghumai thako");// condition before question mark
+
+const price = 500;
+const isLeader = true;
+
