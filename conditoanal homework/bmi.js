185e5195525622e6aa48249f28b1ef2d2aa28a33
Imramul Hoque <imramulhoque01@gmail.com>
2024-02-14 19:48:23 +0600

    bmi

--- conditoanal homework/bmi.js
+++ conditoanal homework/bmi.js
@@ -1,4 +1,4 @@
-const weight = 80;
+const weight = 3 0;
 const height = 1.77;
 const bmi = weight / (height)^2 ;
 if (bmi < 18.5) {
@@ -6,7 +6,10 @@ if (bmi < 18.5) {
 } else if (bmi >=18.5 && bmi <=29.9){
     console.log("you are normal");
 } else if (bmi >= 25 && bmi <= 29.9){
-    console.log("you are overweight");
+    bmi >= 25 && bmi <= 29.9
 }else {
     console.log('you are obese');
-}
+} 
+
+bmi< 18.5 ?   console.log("you are underwighte") : bmi >=18.5 && bmi <=29.9 ? console.log("you are normal") :
+ bmi >= 25 && bmi <= 29.9 ? bmi >= 25 && bmi <= 29.9 : console.log('you are obese');
---
+++ conditoanal homework/grade.js
@@ -0,0 +1,13 @@
+const scoore = 80;
+let grade;
+if (scoore >= 90) {
+    console.log(" grade  A");
+} else if ( scoore >= 80){
+    console.log(" grade  B");
+} else if ( scoore >= 70){
+    console.log(" grade  C");
+} else if ( scoore >= 60){
+    console.log(" grade  D");
+} else if ( scoore >= 50){
+    console.log(" grade  E");
+}
